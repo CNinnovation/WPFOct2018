3a606e4ed779dca7cefc8cd2cc85399f0c574017
Christian Nagel <christian@christiannagel.com>
2018-10-24 12:55:32 +0200

    pattern matching

---
+++ PatternMatchingSample/PatternMatchingSample/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternMatchingSample
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ PatternMatchingSample/PatternMatchingSample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace PatternMatchingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternsSample();
+        }
+
+        static void PatternsSample()
+        {
+            object[] data = { 42, "astring", null, new Person() { FirstName = "Katharina", LastName = "Nagel" } };
+            foreach (var item in data)
+            {
+                // IsSample(item);
+                SwitchSample(item);
+            }
+        }
+
+        static void IsSample(object o)
+        {
+            // if (o is null) throw new ArgumentNullException(nameof(o));
+
+            if (o is 42)  // const pattern
+            {
+                Console.WriteLine("it's 42");
+            }
+
+            if (o is int i)  // type pattern
+            {
+                Console.WriteLine($"it's an int with {i}");
+            }
+
+            if (o is Person p && p.LastName == "Nagel")
+            {
+                Console.WriteLine($"person {p.FirstName}");
+            }
+
+            if (o is var v)  // var pattern
+            {
+                Console.WriteLine($"it's var of type {v?.GetType().Name}");
+            }
+        }
+
+        static void SwitchSample(object o)
+        {
+            switch (o)
+            {
+                case 42: // const pattern
+                    Console.WriteLine("42");
+                    break;
+                case null: // const pattern
+                    Console.WriteLine("null");
+                    break;
+                case int i: // type pattern
+                    Console.WriteLine($"int {i}");
+                    break;
+
+                case Person p when p.FirstName.StartsWith("Kat"):
+                    Console.WriteLine($"person {p}");
+                    break;
+                case Person p:
+                    Console.WriteLine("other person");
+                    break;
+                //case var v:
+                //    Console.WriteLine("always var");
+                //    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
