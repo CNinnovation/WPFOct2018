fbe0a3b6c039850c244b0698a4970ac8ab5ad435
Christian Nagel <christian@christiannagel.com>
2018-10-24 12:55:08 +0200

    dependency properties

---
+++ DependencyPropertySample/DependencyPropertySample/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyPropertySample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button b = new Button();
+        }
+
+        private int _x;
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+
+
+        public int Y
+        {
+            get { return (int)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Y.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register("Y", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+    }
+}
