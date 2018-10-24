7be1c7126a4fcfd95718806c177a156c2680b287
Christian Nagel <christian@christiannagel.com>
2018-10-24 12:55:19 +0200

    tuples

---
+++ TupleSample/TupleSample/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleSample
+{
+    public class Person
+    {
+        public Person(string first, string last) => (FirstName, LastName) = (first, last);
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        //public void Deconstruct(out string first, out string last) =>
+        //    (first, last) = (FirstName, LastName);
+    }
+
+    public static class PersonExtensions
+    {
+        public static void Deconstruct(this Person p, out string first, out string last) =>
+            (first, last) = (p.FirstName, p.LastName);
+    }
+}
---
+++ TupleSample/TupleSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TupleSample
+{
+    readonly struct MyStruct
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = Tuple.Create("hello", 42);
+            string s1 = t1.Item1;
+            int i1 = t1.Item2;
+            // t1.Item1 = "test";
+
+            var t2 = (s: "magic", i: 42, p: new Person("Stephanie", "Nagel"));  // ValueTuple
+            t2.s = "more magic";
+
+
+            var person = new Person("Matthias", "Nagel");
+            // deconstruction
+            (string mystring, int myval) = ("magic", 42);
+
+            (string first, string last) = person;
+
+            Console.WriteLine(first);
+
+
+        }
+    }
+}
