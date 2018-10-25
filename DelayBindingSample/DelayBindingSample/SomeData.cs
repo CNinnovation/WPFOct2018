35d3d58148a8753467db2643c8fba3e5b0e83283
Christian Nagel <christian@christiannagel.com>
2018-10-25 13:09:22 +0200

    delay binding

---
+++ DelayBindingSample/DelayBindingSample/SomeData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DelayBindingSample
+{
+    public class SomeData
+    {
+        public SomeData()
+        {
+            Info = new ObservableCollection<string>();
+            _someText = "init";
+        }
+
+        private string _someText;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                Info.Add(value);
+                _someText = value;
+            }
+        }
+
+        public ObservableCollection<string> Info { get; set; }
+
+    }
+}
