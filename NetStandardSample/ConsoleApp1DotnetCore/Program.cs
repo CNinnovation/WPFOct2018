b91fc4f5d8343dd5b41ff6836883bdbb554c9fec
Christian Nagel <christian@christiannagel.com>
2018-10-25 13:09:04 +0200

    .net standard

---
+++ NetStandardSample/ConsoleApp1DotnetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using NetStandardSample;
+using System;
+
+namespace ConsoleApp1DotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var std = new MyDotnetStandard();
+            string result = std.JustAMethod("hello");
+            Console.WriteLine(result);
+            std.ShowMessage("hello");
+        }
+    }
+}
---
+++ NetStandardSample/NetStandardSample/MyDotnetStandard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TheReallyOldLib;
+
+namespace NetStandardSample
+{
+    public class MyDotnetStandard
+    {
+        public string JustAMethod(string s)
+        {
+            var d = new DotnetFramework();
+            return d.ReturnAString(s);
+        }
+
+        public void ShowMessage(string s)
+        {
+            var d = new DotnetFramework();
+            d.ShowAMessage(s);
+        }
+
+    }
+}
